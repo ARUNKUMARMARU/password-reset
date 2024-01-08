c5d1d3df90279d029ff36ce08897ba5b6175ee9e
ARUNKUMARMARU <arunsmart1020@gmail.com>
2024-01-08 16:29:06 +0530

    check

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const config  = require('./util/config');
 const app = require('./app');
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the App')
+  })
+
 app.listen(config.PORT, ()=>{
+
 
     console.log(`Running server port is ${config.PORT}`);
     mongoose.connect(config.MONGODB_URI)
